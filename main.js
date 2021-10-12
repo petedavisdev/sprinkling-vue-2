17496b898bfb2d04a64b56499af6c45668cf061a
Pete Davis <pete.davis@rcn.org.uk>
2021-10-12 14:01:05 +0100

    Add vue functionality comments

---
+++ main.js
@@ -0,0 +1,19 @@
+/* TODO 2: import vue from CDN
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js";*/
+// TODO 7: import component objects
+import { clickCounter } from "./components/clickCounter.js";
+
+/* TODO 8: register vue components
+Vue.component("ClickCounter", clickCounter);*/
+
+/* TODO 3: create vue instances
+const vueApps = document.querySelectorAll("[data-vue]");
+
+vueApps.forEach((app) => {
+    const appData = JSON.parse(app.dataset.vue || "{}");
+
+    new Vue({
+        el: app,
+        data: appData,
+    });
+});*/
